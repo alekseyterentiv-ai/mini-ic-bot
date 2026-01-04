7772010c7c16da0de6f6ff878b1d57376b18703b
alekseyterentiv-ai <alekseyterentiv@gmail.com>
2026-01-05 00:20:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, request
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.get("/")
 def index():
@@ -14,6 +14,6 @@ def webhook():
     print(json.dumps(data, ensure_ascii=False))
     return "ok", 200
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
